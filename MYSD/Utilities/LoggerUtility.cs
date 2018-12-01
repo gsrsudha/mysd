2dbb1ae0d832fce73f198756b614df14be175487
gsrsudha <gsr.sudha8@gmail.com>
2018-11-30 21:20:23 -0600

    Add project files.

---
+++ MYSD/Utilities/LoggerUtility.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MYSD.Utilities
+{
+    public class LoggerUtility
+    {
+        public static ILog Logger;
+
+        internal static void InitializeLogger()
+        {
+            if (LogManager.GetCurrentLoggers().Length == 0)
+            {
+                log4net.Config.BasicConfigurator.Configure();
+            }
+            Logger = LogManager.GetLogger("MYSDLogger");
+            Logger.Info("Make your special day System - Application started.");
+        }
+    }
+}
