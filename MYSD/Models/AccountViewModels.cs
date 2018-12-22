21c2473bbe54b032a2bb1246d6c4f40167556a42
gsrsudha <gsr.sudha8@gmail.com>
2018-12-21 19:26:41 -0600

    added users roles

--- MYSD/Models/AccountViewModels.cs
+++ MYSD/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace MYSD.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -68,6 +67,14 @@ namespace MYSD.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "UserRoles")]
+        public string UserRoles { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
